8a54a1edb63eafe49326ea325169427856eb317b
1DoraGON <moulaykadii2001@gmail.com>
2023-07-18 13:31:55 +0200

    initialize project

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+    return (
+      <h1 className="text-3xl bg-slate-300 font-bold underline">
+        Hello world!
+      </h1>
+    )
+  
+}
+
+export default App

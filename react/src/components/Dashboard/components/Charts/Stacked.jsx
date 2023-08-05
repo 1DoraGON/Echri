a4a1d3174c5a755f36394b253b036beb1cb3a99f
1DoraGON <moulaykadii2001@gmail.com>
2023-08-05 14:54:08 +0200

    orders page done along with ecommerce page

--- react/src/components/Dashboard/components/Charts/Stacked.jsx
+++ react/src/components/Dashboard/components/Charts/Stacked.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Category, ChartComponent, Inject, Legend, SeriesCollectionDirective, SeriesDirective, StackingColumnSeries, Tooltip } from "@syncfusion/ej2-react-charts"
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from "../../../../data/dummy"
+const Stacked = ({ width, height }) => {
 
-const Stacked = () => {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{background:'white'}}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective >
+        {stackedCustomSeries.map((item, i) => <SeriesDirective key={i} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 

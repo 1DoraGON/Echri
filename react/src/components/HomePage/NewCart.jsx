70f194a3b5ba66c0a9e0dc6045de705c9224d5ad
1DoraGON <moulaykadii2001@gmail.com>
2023-09-03 22:46:34 +0200

    working

--- react/src/components/HomePage/NewCart.jsx
+++ react/src/components/HomePage/NewCart.jsx
@@ -15,6 +15,8 @@ import SuccessAlert from '../utils/SuccessAlert'
 import useAuth from '../../hooks/useAuth'
 import CartLabel from '../Cart/CartLabel'
 import { useParams } from 'react-router-dom'
+import OrderItem from '../Cart/OrderItem'
+import ConfirmationModal from '../utils/ConfirmationModal'
 
 const NewCart = () => {
   const wilayas = algerianStates
@@ -66,6 +68,7 @@ const NewCart = () => {
             setEnableEdit(false)
           }
           const form = {
+            id: data.id,
             firstname: data.firstname,
             lastname: data.lastname,
             number: data.phone_number,
@@ -82,7 +85,7 @@ const NewCart = () => {
           //console.log(form);
           setFormData(form)
           setOrderProducts(data.products)
-          console.log(response);
+          console.log(form);
         }).catch(error => {
           console.log(error);
         })
@@ -153,6 +156,9 @@ const NewCart = () => {
       }
     })
   }
+
+
+  //+ wilayas.find(wilaya => wilaya.id === parseInt(formData.wilaya, 10)).name
   return (
 
 
@@ -171,7 +177,7 @@ const NewCart = () => {
           <div className="rounded-lg w-2/3 md:w-full">
             {id ?
 
-              formData.products.map((item, i) => (
+              orderProducts.map((item, i) => (
                 <OrderItem key={i} item={item} />
               ))
               :
@@ -184,6 +190,55 @@ const NewCart = () => {
 
 
           <div className="md:mt-6 md:w-full h-full rounded-lg border bg-white p-6 shadow-md mt-0 w-1/2 mx-5">
+            {id && (
+              <>
+                <div className="mb-2 w-full flex justify-end">
+                  <div className="self-end">
+
+                    <button
+                      id="deleteButton"
+                      data-modal-toggle="deleteOrder"
+                      type="button"
+                      className="text-red-500 hover:text-red-600 rounded-md flex items-center space-x-1 focus:outline-none focus:ring focus:ring-red-200"
+                    >
+                      <span className='text-xl mr-1'>&times;</span>
+                      Cancel Order
+                    </button>
+                  </div>
+                </div>
+                <ConfirmationModal id={'deleteOrder'} text={'Are you sure you want to cancel this order?'} confirmation={'Yes, I\'m sure'} cancel={'No, Keep it'} />
+              </>
+            )}
+
+            
+            <div className="flex justify-center m-5">
+              <button id="deleteButton" data-modal-toggle="deleteModal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                Show delete confirmation
+              </button>
+            </div>
+
+            
+            <div id="deleteModal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+              <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+                
+                <div className="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                  <button type="button" className="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="deleteModal">
+                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                  <svg className="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd"></path></svg>
+                  <p className="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want to delete this item?</p>
+                  <div className="flex justify-center items-center space-x-4">
+                    <button data-modal-toggle="deleteModal" type="button" className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                      No, cancel
+                    </button>
+                    <button type="submit" className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                      Yes, I'm sure
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
             {id && (
               <div className="mb-2 flex justify-between">
                 <p className="text-gray-700">Order ID</p>
@@ -218,7 +273,7 @@ const NewCart = () => {
                   <CartLabel value={formData.firstname} label={'Firstname'} />
                   <CartLabel value={formData.lastname} label={'Lastname'} />
                   <CartLabel value={formData.number} label={'Phone Number'} />
-                  <CartLabel value={formData.wilaya + ' - ' + wilayas.find(wilaya => wilaya.id === parseInt(formData.wilaya, 10)).name} label={'Wilaya'} />
+                  <CartLabel value={formData.wilaya + ' - '} label={'Wilaya'} />
                   <CartLabel value={formData.full_address} label={'Full Address'} />
                   <CartLabel value={formData.payment_method} label={'Payment Method'} />
                   <CartLabel value={formData.home_delivery ? 'Home Delivery' : 'Yalidine Center'} label={'Delivery Type'} />
